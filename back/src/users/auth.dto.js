0159b9c8765d65f3304f06dc6b447909e6ee3109
Mickael-Desclaux <mickaeldesclaux@gmail.com>
2026-02-19 12:15:23 +0100

    messages d'erreurs in english, for sure

--- back/src/users/auth.dto.js
+++ back/src/users/auth.dto.js
@@ -2,13 +2,13 @@ import z from 'zod'
 
 export const passwordSchema = z
   .string()
-  .min(8, 'Min 8 caractères')
-  .regex(/[a-z]/, 'Le mot de passe doit contenir au moins une minuscule')
-  .regex(/[A-Z]/, 'Le mot de passe doit contenir au moins une majuscule')
-  .regex(/[0-9]/, 'Le mot de passe doit contenir au moins un chiffre')
+  .min(8, 'Minimum 8 characters')
+  .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
+  .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+  .regex(/[0-9]/, 'Password must contain at least one digit')
   .regex(
     /[^A-Za-z0-9]/,
-    'Le mot de passe doit contenir au moins un caractère spécial'
+    'Password must contain at least one special character'
   )
 
 export const RegisterDTO = z.object({
