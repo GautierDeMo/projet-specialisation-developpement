e0124a219daea4b9e3abe212d2bd2cd91e50445a
Gautier de MAUROY <89637837+GautierDeMo@users.noreply.github.com>
2026-02-21 17:45:34 +0100

    ♻️ refactor(utils): update urlToBase64 to use arrayBuffer
    🔧 utils.js: change buffer fetching method to arrayBuffer and update conversion to base64

--- back/src/utils/utils.js
+++ back/src/utils/utils.js
@@ -1,5 +1,5 @@
 export async function urlToBase64(url) {
   const response = await fetch(url)
-  const buffer = await response.buffer()
-  return buffer.toString('base64')
+  const arrayBuffer = await response.arrayBuffer()
+  return Buffer.from(arrayBuffer).toString('base64')
 }
